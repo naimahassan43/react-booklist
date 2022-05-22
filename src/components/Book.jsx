1e828c67a6698fab1cae63aa18677917fd9232be
Naima Hassan <naima.hassan434@gmail.com>
2022-05-22 02:31:01 +0100

    events practice

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -3,6 +3,13 @@ import "./book.css";
 
 const Book = ({ title, author, img, children }) => {
   // const { title, author, img, children } = props;
+  const clickHandler = () => {
+    alert('Click event fired')
+  }
+  const clickAuthor = (author) => {
+    
+    alert("Author Name: " + author);
+  }
   return (
     <article className="book">
       <img src={img} alt={title} style={{ height: "400px", width: "250px" }} />
@@ -13,6 +20,20 @@ const Book = ({ title, author, img, children }) => {
         {author}
       </h4>
       {children}
+      <button
+        style={{ marginTop: "0.5rem", cursor: "pointer" }}
+        type="button"
+        onClick={clickHandler}
+      >
+        Click here
+      </button>
+      <button
+        style={{ marginTop: "0.5rem", cursor: "pointer" }}
+        type="button"
+        onClick={()=>clickAuthor(author)}
+      >
+        Author Name
+      </button>
     </article>
   );
 };
